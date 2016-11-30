2cfd834bc3b9e733f0219673e4df450e85a8f963
Jacek Polom <Jacek.Polom-External@hamburgsud.com>
2016-11-30 09:24:43 +0100

    Remove unnecessary code and imports

--- src/main/java/com/polomos/config/AppInitializer.java
+++ src/main/java/com/polomos/config/AppInitializer.java
@@ -1,14 +1,13 @@
 package com.polomos.config;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 
 
 public class AppInitializer implements WebApplicationInitializer {
--- src/main/java/com/polomos/controller/UserController.java
+++ src/main/java/com/polomos/controller/UserController.java
@@ -1,7 +1,5 @@
 package com.polomos.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +7,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.polomos.dao.model.User;
 import com.polomos.service.UserService;
 
 
